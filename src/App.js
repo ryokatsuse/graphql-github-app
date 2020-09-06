78e576240190c54ed63ca61d855213de4c41ae0f
Ryo Katsuse <ryokatsu0719@gmail.com>
2020-09-06 23:21:18 +0900

    feat:apllo-boostで自分のアイコンを取得して表示する

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react'
+import { ApolloProvider } from 'react-apollo'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import client from './client'
 
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-console.log(GITHUB_TOKEN)
+const ME = gql`
+  query me {
+    user(login: "ryokatsuse") {
+      name
+      avatarUrl
+    }
+  }
+`
 
 function App() {
   return (
-    <div className="App">
+    <ApolloProvider client={client} >
+    <div>
       <p>hello</p>
+      <Query query={ME}>
+        {
+          ({loading, error, data}) => {
+            if(loading) return 'Loading...'
+            if(error) return `Error! ${error}`
+
+            return <img src={data.user.avatarUrl} />
+          }
+        }
+      </Query>
     </div>
+    </ApolloProvider>
   );
 }
 
